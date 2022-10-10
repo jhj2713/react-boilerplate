929a599c5693dce444999e01f9752d9ec7676585
juhyojeong <juhyojeong58@gmail.com>
2022-10-10 13:09:53 +0900

    chore: 경로 수정 및 gitignore 추가

--- webpack.common.js
+++ webpack.common.js
@@ -29,7 +29,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "public/index.html",
+      template: path.resolve(__dirname, "public", "index.html"),
     }),
   ],
 };
