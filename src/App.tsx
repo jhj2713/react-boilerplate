6b77f42840746ef0867f4e1273b077848a938bfa
juhyojeong <juhyojeong58@gmail.com>
2022-10-10 12:59:02 +0900

    fix: babel/runtime-corejs3 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import styled from "styled-components";
+
 const App = () => {
-  return <div>React Boilerplate</div>;
+  return (
+    <Container>
+      <Text>React Boilerplate</Text>
+    </Container>
+  );
 };
 
 export default App;
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Text = styled.p`
+  margin: 0;
+
+  color: #587cf7;
+`;
